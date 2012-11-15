23e80f07409baa7ca3637825b62fe86f412a5787
Josyan Pierson <josyan.pierson@gmail.com>
2012-11-15 12:41:24 -0500

    Example 1-1

---
+++ helloworld.js
@@ -0,0 +1,7 @@
+var http = require('http');
+
+http.createServer(function(req, res) {
+  res.writeHead(200, {'content_type': 'text/plain'});
+  res.end('Hello World\n');
+}).listen(8124);
+console.log('Server running on 8124');
