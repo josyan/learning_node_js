9b6f30d9b266b98783e566d8317c3183296c9b90
Josyan Pierson <josyan.pierson@gmail.com>
2012-11-19 12:37:29 -0500

    Example 1-4 take 2

--- example-1-3.js
+++ example-1-3.js
@@ -1,8 +1,9 @@
 var http = require('http');
 var fs = require('fs');
 
+var counter = 0;
+
 function writeNumbers(res) {
-  var counter = 0;
   for(var i = 0; i < 100; i++) {
     counter++;
     res.write(counter.toString() + '\n');
